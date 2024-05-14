1f9a80153d3edcc12cee6d17e91fb31b37ef7f12
Cyrus Truitt <cyrus.truitt@mail.com>
2024-05-14 18:58:33 -0400

    Add files via upload

---
+++ Toman Bike Share.sql
@@ -0,0 +1,29 @@
+
+Use toman_bike
+
+
+GO
+
+
+-- Join Tables
+with cte as (
+select * from bike_share_yr_0
+union
+select * from bike_share_yr_1)
+
+
+select
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price,
+COGS,
+riders*price as revenue,
+riders*price -COGS as profit
+from cte a
+left join cost_table b
+on a.yr = b.yr
